a3a26835bd275d131c1fd90eece3616fa3af702d
Marco Moauro <marco.moauro@translated.net>
2024-05-21 10:14:31 +0200

    remoce schema sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-create table _newsletters_template
-(
-    id          bigserial primary key,
-    description varchar(255)                        null,
-    created_at  timestamp default CURRENT_TIMESTAMP not null,
-    updated_at  timestamp default CURRENT_TIMESTAMP not null
-);
